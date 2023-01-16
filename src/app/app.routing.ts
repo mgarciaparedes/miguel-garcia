62c0783ef836fe89128407702ff961d6dc937b86
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2023-01-16 20:01:15 -0300

    fix: get in touch with mailto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import { Router, RouterModule, Routes } from '@angular/router';
 //Importar componentes a redireccionar
 import { BodyComponent } from './components/body/body.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { GetInTouchComponent } from './components/get-in-touch/get-in-touch.component';
+// import { GetInTouchComponent } from './components/get-in-touch/get-in-touch.component';
 import { Error404Component } from './components/error404/error404.component';
 
 //Array de rutas
@@ -12,7 +12,7 @@ const appRoutes: Routes = [
   { path: '', component: BodyComponent },
   { path: 'home', component: BodyComponent },
   { path: 'projects', component: ProjectsComponent },
-  { path: 'get-in-touch', component: GetInTouchComponent },
+  // { path: 'get-in-touch', component: GetInTouchComponent },
   { path: '**', component: Error404Component },
 ];
 
